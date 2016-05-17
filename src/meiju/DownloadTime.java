8352fed1d885fb716f7f0206318406a75141d180
flonghk <huangk_2009@126.com>
2016-05-17 17:57:41 +0800

    the month of Calender add 1

--- src/meiju/DownloadTime.java
+++ src/meiju/DownloadTime.java
@@ -11,7 +11,7 @@ public class DownloadTime {
 		Calendar calendar = Calendar.getInstance();//可以对每个时间域单独修改
 
 		int year = calendar.get(Calendar.YEAR); 
-		int month = calendar.get(Calendar.MONTH); 
+		int month = calendar.get(Calendar.MONTH)+1; 
 		int date = calendar.get(Calendar.DATE); 
 		int hour = calendar.get(Calendar.HOUR_OF_DAY); 
 		int minute = calendar.get(Calendar.MINUTE); 
